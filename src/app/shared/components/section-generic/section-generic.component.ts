2650fed1fe15c0939017efb30780a55d3095a7d4
MoiZuleta98 <moiseszuleta02@hotmail.com>
2023-06-18 23:27:08 -0300

    empezamos a usar el decorador input para section-generic

--- src/app/shared/components/section-generic/section-generic.component.ts
+++ src/app/shared/components/section-generic/section-generic.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-section-generic',
   templateUrl: './section-generic.component.html',
   styleUrls: ['./section-generic.component.css']
 })
-export class SectionGenericComponent {
+export class SectionGenericComponent implements OnInit {
+  @Input() title: string = ''  //Declaramos un decorador de un input. El decorador marca un campo de clase como propiedad de entrada y proporciona metadatos de configuración 
+
+  constructor() {  }
+
+  ngOnInit(): void {
+  }
 
 }
